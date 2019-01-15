fdb7136a416b1d2b8c8206e6f0f664404b850295
Kevin Yang <yangke@umich.edu>
2019-01-14 22:11:42 -0500

    use my fork of betterscreenlocker so suspend-lock is managed by xss-lock for avoiding systemd

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,46 +1,44 @@
 #!/usr/bin/env sh
+betterlockscreen_path="$HOME/src/betterlockscreen/betterlockscreen"
 
 i3lock_wrapper() {
-  i3lock --ignore-empty-password --color=071715 && sleep 1
+  i3lock -u --ignore-empty-password --color=071715 && sleep 1
 }
-lock() {
-  if [ -x ~/.bin/betterlockscreen ]; then
-    ~/.bin/betterlockscreen --lock dimblur
+
+betterlockscreen_wrapper() {
+  $betterlockscreen_path --lock dimblur -- -u
+}
+
+lock_lock() {
+  if [ -x $betterlockscreen_path ]; then
+    betterlockscreen_wrapper
   else
     i3lock_wrapper
   fi
 }
 
-suspend() {
-  if [ -x ~/.bin/betterlockscreen ]; then
-    ~/.bin/betterlockscreen --suspend dimblur
-  else
-    i3lock_wrapper && systemctl suspend
-  fi
-}
-
-hibernate() {
-  if [ -x ~/.bin/betterlockscreen ]; then
-    ~/.bin/betterlockscreen --lock dimblur && systemctl hibernate
+lock_hibernate() {
+  if [ -x $betterlockscreen_path ]; then
+    betterlockscreen_wrapper && systemctl hibernate
   else
     i3lock_wrapper && systemctl hibernate
   fi
 }
 
-while getopts "lsh" opt; do
-  case $opt in
-    l)
-      lock
-      ;;
-    s)
-      suspend
-      ;;
-    h)
-      hibernate
-      ;;
-    \?)
-      lock
-      ;;
-  esac
-done
-
+if [ "$#" -eq 0 ]; then
+  lock_lock
+else
+  while getopts "lsh" opt; do
+    case $opt in
+      l)
+        lock_lock
+        ;;
+      h)
+        lock_hibernate
+        ;;
+      \?)
+        lock_lock
+        ;;
+    esac
+  done
+fi
